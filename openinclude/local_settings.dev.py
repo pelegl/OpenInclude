61289b046f2dbddc8fa1336af70bc6f312d069fa
Zhu Tao <zhutao.iscas@gmail.com>
2012-11-22 16:10:36 +0800

    fix an import issue

--- openinclude/local_settings.dev.py
+++ openinclude/local_settings.dev.py
@@ -1,8 +1,10 @@
+import os
+from django.conf import settings
 DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': path.join(PROJECT_ROOT, 'oi.db'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(settings.PROJECT_ROOT, 'oi.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
