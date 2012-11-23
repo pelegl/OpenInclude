987df14cf01888a059dc3cd0ebfa6ac079431e6c
Bill Dvorak <william.k.dvorak@gmail.com>
2012-11-23 14:19:39 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 ssh -i /Users/williamdvorak/Documents/Business/OpenInclude/deployment/dvorak.pem ubuntu@ec2-50-17-38-198.compute-1.amazonaws.com
 git pull https://github.com/skier31415/OpenInclude
 python manage.py runserver 0.0.0.0:8000
+ec2-50-17-38-198.compute-1.amazonaws.com:8000
