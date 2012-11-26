04a21a091bf27d1c8e6bf95a2b979177a487617c
Zhu Tao <zhutao.iscas@gmail.com>
2012-11-26 09:54:28 +0800

    add tags model

--- openinclude/tag/models.py
+++ openinclude/tag/models.py
@@ -6,10 +6,31 @@ from djangosphinx import SphinxSearch
 
 # import our local imports here
 from django.contrib.auth.models import User
+from project.models import Project
 
-class Member(models.Model):
-    user = models.ForeignKey(User, related_name="MemberUser")
-    github_username = models.CharField(max_length=20)
-    
+class TagManager(models.Manager):
+    pass
+
+class Tag(models.Model):
+    """Tag model
+    """
+    name = models.CharField(max_length=30)
+
+    objects = TagManager()
+
     def __unicode__(self):
-        return self.github_username
+        return self.name
+
+class ProjectTagManager(models.Manager):
+    pass
+
+class ProjectTag(models.Model):
+    project = models.ForeignKey(Project, related_name="ProjectTagProject")
+    tag = models.ForeignKey(Tag, related_name="ProjectTagTag")
+
+    objects = ProjectTagManager()
+        
+
+    def __unicode__(self):
+        return "%s-%s" % (self.project, self.tag)
+
