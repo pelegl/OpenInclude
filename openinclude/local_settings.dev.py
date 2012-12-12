b35f814a8b294ac644558481f576524ba36c948c
Zhu Tao <zhutao.iscas@gmail.com>
2012-12-12 09:17:10 +0800

    refine the local settings

--- openinclude/local_settings.dev.py
+++ openinclude/local_settings.dev.py
@@ -16,6 +16,3 @@ DATABASES = {
 CLIENT_ID = "234786241ad0cd94fce3"
 CLIENT_SECRET = "4fd8f415a013eb08691458bdc89be5d281cabd53"
 REDIRECT_URL = "http://127.0.0.1:8000/member/after_login/"
-CLIENT_ID = "f416583827e22efa2986"
-CLIENT_SECRET = "a7ef060d3a669a568dd97f994eaa47ec27a993f3"
-REDIRECT_URL = "http://ec2-184-73-51-98.compute-1.amazonaws.com/member/after_login/"
