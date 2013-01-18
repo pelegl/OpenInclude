5cf11fc54d49accdf48efc1db21bf1ea858f69cb
Aurlus Ishmael <arlusishmael@gmail.com>
2013-01-18 13:51:22 +0300

    excluded fork repos

--- scrapers/scrapers/spiders/github_api.py
+++ scrapers/scrapers/spiders/github_api.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-# RUN THIS SCRIPT INDEPENDENT OF SCRAPY
+#RUN THIS SCRIPT INDEPENDENT OF SCRAPY
 import urllib2
 import simplejson
 import pymongo
 import time
 
-# retry decorator
+#retry decorator
 def retry(ExceptionToCheck, tries=10, delay=5, backoff=2, logger=None):
     """Retry calling the decorated function using an exponential backoff.
 
@@ -49,25 +49,25 @@ def retry(ExceptionToCheck, tries=10, delay=5, backoff=2, logger=None):
             return
         return f_retry  # true decorator
     return deco_retry
-# end of retry decorator
+#end of retry decorator
 
-# user agent to aoid blacklisting
+#user agent to aoid blacklisting
 USER_AGENT = 'Mozilla/5.0'
-# github command line token to allow 5000 fetches per hour...non auth requests
-# allowd are only 60 per hour
+#github command line token to allow 5000 fetches per hour...non auth requests
+#allowd are only 60 per hour
 AUTH_TOKEN = 'token 983108819e9f8a7709d0029d66939e7f2b5122ab'
 
-# database with language list
+#database with language list
 connection = pymongo.Connection()
 db = connection['github_languages']
 collection = db['language_names']
 
-# database to save module details
+#database to save module details
 data_connection = pymongo.Connection()
 database = data_connection['github_modules']
 modules_collection = database['modules']
 
-# fetching the language list
+#fetching the language list
 lang_list = list(collection.find())
 language_list = []
 
@@ -77,18 +77,18 @@ for lang in lang_list:
 
 print language_list
 
-# headers for the request
+#headers for the request
 hdr = {'User-Agent': USER_AGENT, 'Authorization': AUTH_TOKEN }
 
 @retry((urllib2.HTTPError, urllib2.URLError), tries=10, delay=10)
 def url_open_with_retry(request, opener):
     return opener.open(request)
-# For each language in the language list:
+#For each language in the language list:
 for language in language_list:
     count = 1
     status = True
     while status:
-        # get its repos
+        #get its repos
         address = "https://api.github.com/legacy/repos/search/:language=" + language + "?start_page=" + str(count)
         print address
         request = urllib2.Request(address, headers=hdr)
@@ -97,21 +97,19 @@ for language in language_list:
         modules_json = simplejson.load(response)
         modules_list = modules_json['repositories']
         if modules_json['repositories'] == []:
-            # stop incrementing pages if it starts returning empty results
+            #stop incrementing pages if it starts returning empty results
             status = False
         else:
-            # for each repo
+            #for each repo
             for module in modules_list:
                 print module['username'] + "\n"
                 print address
-                if module['fork'] == "true":
-                    pass
-                else:
-                # save its repos' details
-                    insert_module_details = modules_collection.insert({'module_name': module['name'], 'owner': module['owner'],
+                #save its repos' details
+                insert_module_details = modules_collection.insert({'module_name': module['name'], 'owner': module['owner'],
                                                                    'description': module['description'], 'language': language, 'watchers': module['watchers'],
                                                                    'pushed_at': module['pushed_at'], 'forks': module['forks'], 'created': module['created'],
                                                                    'pushed': module['pushed'], 'created': module['created'], 'is_a_fork': module['fork'],
                                                                    'followers': module['followers'], 'username':module['username']})
         count += 1
+
 
