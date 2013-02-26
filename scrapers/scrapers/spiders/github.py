5b97f3d6813d07eedc92fc895e571fcf2add2337
w3 <w3s.provider@gmail.com>
2013-02-26 15:46:05 +0530

    add cutom exception for testing the code

--- scrapers/scrapers/spiders/github.py
+++ scrapers/scrapers/spiders/github.py
@@ -109,8 +109,8 @@ class language_spider(BaseSpider):
         
     def parse(self, response):
         urls = []
+        print(response)
         hxs = HtmlXPathSelector(response)
-        print(hxs)
         raise Exception('end')
         languages = hxs.select('//*[@id="languages"]/div/div[1]/div/ul/li//text()').extract()
         del languages[0]
