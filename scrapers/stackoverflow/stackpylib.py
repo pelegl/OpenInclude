1f7803f1dc8e7c0506126f6c92f92c6231af4909
aleks1k <aleks.kn@gmail.com>
2013-03-09 14:07:54 +0400

    search for modules name

--- scrapers/stackoverflow/stackpylib.py
+++ scrapers/stackoverflow/stackpylib.py
@@ -1,11 +1,32 @@
 __author__ = 'Alexey'
 
-from stackpy import Site
+import stackpy
+import pymongo
 
-site = Site('stackoverflow')
-i=0
-# for question in site.questions.filter('withbody'):
-for question in site.search('advanced').url('*github.com/twitter*').filter('withbody'):
-    i += 1
-    print '%d --- %s ---' % (i, question.title)
-    print question.body
+MONGODB_IP = 'ec2-107-20-8-160.compute-1.amazonaws.com'
+site = stackpy.Site('stackoverflow')
+
+#mongoConn = pymongo.MongoClient(MONGODB_IP, 27017)
+mongoConn = pymongo.MongoClient()
+
+db = mongoConn['openIncludeCopy']
+modules = db['modules']
+db.drop_collection('module_so2')
+module_so = db['module_so2']
+try:
+    for module in modules.find().sort('watchers', pymongo.DESCENDING).limit(100):
+        name = str(module['module_name']).lower()
+        print name,
+        i = 0
+        for question in site.search('advanced').pagesize(100).url('*github.com/*/%s*' % name).filter('_bca'):
+            i += 1
+            question._data['module_id'] = module['_id']
+            module_so.insert(question._data)
+            # print '%d --- %s ---' % (i, question.title)
+            # if i >= 100:
+            #     break
+            # print '.',
+        print i
+except stackpy.url.APIError as ex:
+    print ex.message
+
