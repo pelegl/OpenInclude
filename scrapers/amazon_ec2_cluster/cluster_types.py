e0e6d779de6a7fb5d139481aa35394bbe11491a3
aleks1k <aleks.kn@gmail.com>
2013-03-12 12:48:52 +0400

    refactoring

--- scrapers/amazon_ec2_cluster/cluster_types.py
+++ scrapers/amazon_ec2_cluster/cluster_types.py
@@ -39,8 +39,8 @@ class NodeBase:
     def update_curr_node_status(self, status, message = None):
         curr_node = self.get_curr_node()
         if message != None:
-            curr_node['logs'].append('%s:%s' % (str(datetime.now()), message))
-        curr_node['logs'].append('%s:%s->%s' % (str(datetime.now()), curr_node['status'], status))
+            curr_node['logs'].append('%s:%s' % (datetime.now().strftime("%H:%M:%S"), message))
+        curr_node['logs'].append('%s:%s->%s' % (datetime.now().strftime("%H:%M:%S"), curr_node['status'], status))
         curr_node['status'] = status
         self.nodes.save(curr_node)
 
@@ -110,7 +110,7 @@ class NodeBase:
 
         print self.db_ip, self.db_name, self.db_tasks_collection, self.db_nodes_collection
 
-    def __init__(self):
+    def init(self):
         self.mongoConn = pymongo.MongoClient(self.db_ip, 27017)
 
         self.db = self.mongoConn[self.db_name]
@@ -125,6 +125,7 @@ class NodeBase:
         self.node_id = self.nodes.insert(curr_node)
 
     def run(self):
+        self.init()
         while True:
             curr_node = self.get_curr_node()
             status = curr_node['status']
@@ -132,7 +133,177 @@ class NodeBase:
                 self.run_task(curr_node['task_id'])
             elif status == NodeBase.STATUS_STOP:
                 break
-            sleep(1)
+            elif status == NodeBase.STATUS_INIT:
+                self.update_curr_node_status(NodeBase.STATUS_IDLE)
+            sleep(0.1)
 
         self.update_curr_node_status(NodeBase.STATUS_DEAD)
         self.mongoConn.close()
+
+import sys
+sys.path.append( 'D:/Projects/ec2_tools' )
+import ec2
+
+class MasterNodeBase():
+    db_ip = 'localhost'
+    db_name = 'cluster_test'
+    db_tasks_collection = 'cluster_tasks'
+    db_nodes_collection = 'cluster_nodes'
+
+    NODE_SCRIPTS = ['./node_run.py', './cluster_types.py']
+    SSH_INIT_COMMAND = 'sudo aptitude -y install python-setuptools; sudo easy_install pymongo'
+
+    CLUSTER_NAME = "alpha"
+    db = None
+    nodes = None
+    tasks = None
+
+    def init(self):
+        pass
+
+    def __init__(self, db_ip, db_name, nodes_count):
+        self.db_ip = db_ip
+        self.db_name = db_name
+
+        self.init()
+
+        self.NODE_SCRIPT_PARAMS = '--dbIp=%s --dbName=%s --dbTasks=%s --dbNodes=%s' % \
+                                  (self.db_ip, self.db_name, self.db_tasks_collection, self.db_nodes_collection)
+
+        self.CLUSTER_NODES_COUNT = nodes_count
+
+        self.mongoConn = pymongo.MongoClient(self.db_ip, 27017)
+        self.db = self.mongoConn[self.db_name]
+        self.tasks = self.db[self.db_tasks_collection]
+        self.nodes = self.db[self.db_nodes_collection]
+
+    def create_task(self, tasks_collection, task_params):
+        task = {
+            'params': task_params,
+            'node_id': '',
+            'status': Task.STATUS_NEW, # assign, inprocess, completed, error
+            'result': []
+        }
+        tasks_collection.insert(task)
+
+    def add_tasks(self, tasks_collection):
+        pass
+
+    def create_tasks(self):
+        self.db.drop_collection(self.db_tasks_collection)
+        self.tasks = self.db[self.db_tasks_collection]
+        self.add_tasks(self.tasks)
+
+    def run_cluster(self, create=True):
+        if create:
+            self.db.drop_collection(self.db_nodes_collection)
+            self.nodes = self.db[self.db_nodes_collection]
+
+            ec2.create(self.CLUSTER_NAME, self.CLUSTER_NODES_COUNT, 't1.micro')
+
+            cluster = None
+            while cluster == None:
+                cluster = ec2.get_cluster(self.CLUSTER_NAME)
+                if cluster == None:
+                    print 'cluster is creating...'
+                    sleep(5)
+
+            while 1 == ec2.ssh(self.CLUSTER_NAME, self.CLUSTER_NODES_COUNT-1, 'uname'):
+                print 'last node is starting...'
+                sleep(5)
+
+            ec2.ssh_all(self.CLUSTER_NAME, self.SSH_INIT_COMMAND, background=False)
+
+        for f in self.NODE_SCRIPTS:
+            ec2.scp_all(self.CLUSTER_NAME, f)
+
+        ec2.ssh_all(self.CLUSTER_NAME, 'python ' + self.NODE_SCRIPTS[0] + ' ' + self.NODE_SCRIPT_PARAMS, background=True)
+        print 'Ran cluster %s from %d nodes' % (self.CLUSTER_NAME, self.CLUSTER_NODES_COUNT)
+
+    def stop_cluster(self):
+        ec2.shutdown(self.CLUSTER_NAME)
+
+    def make_final_result(self):
+        pass
+
+    def get_nodes(self, statuses):
+        return self.nodes.find({
+            'status': {
+                '$in': statuses
+            }
+        })
+
+    def get_tasks(self, statuses, limit = None):
+        if limit != None:
+            return self.tasks.find({
+                'status': {
+                    '$in': statuses
+                }
+            }).limit(limit)
+        else:
+            return self.tasks.find({
+                'status': {
+                    '$in': statuses
+                }
+            })
+
+    def assign_task(self, node, task):
+        node['task_id'] = task['_id']
+        task['node_id']  = node['_id']
+        node['status'] = NodeBase.STATUS_ASSIGN
+        task['status'] = Task.STATUS_ASSIGN
+        self.tasks.save(task)
+        self.nodes.save(node)
+        print 'Assign task %s -> node %s' % (task['_id'], node['_id']),
+
+    def init_all_nodes(self):
+        self.nodes.update({
+            'status': {
+                '$in': [NodeBase.STATUS_IDLE,
+                        NodeBase.STATUS_ASSIGN, NodeBase.STATUS_INPROGRESS]
+            }}, {
+            '$set': {
+                'status': NodeBase.STATUS_INIT}})
+
+    def run(self):
+        self.init_all_nodes()
+        qus_count = self.get_tasks([Task.STATUS_COMPLETED]).count()
+        while True:
+            nodes_list = self.get_nodes([NodeBase.STATUS_IDLE])
+            count = nodes_list.count()
+            if count > 0:
+                tasks_list = self.get_tasks([Task.STATUS_NEW], count)
+                if tasks_list.count() > 0:
+                    i=0
+                    for node in nodes_list:
+                        print '\n', qus_count,
+                        self.assign_task(node, tasks_list[i])
+                        qus_count+=1
+                        i += 1
+                        if i >= tasks_list.count():
+                            break
+                else:
+                    tasks_list = self.get_tasks([Task.STATUS_NEW, Task.STATUS_ASSIGN, Task.STATUS_INPROGRESS], 1)
+                    nodes_list = self.get_nodes([NodeBase.STATUS_ASSIGN, NodeBase.STATUS_INPROGRESS])
+                    if tasks_list.count() == 0 and nodes_list.count() == 0:
+                        print '\nall tasks completed'
+                        break
+            else:
+                nodes_list = self.get_nodes([NodeBase.STATUS_IDLE,
+                                             NodeBase.STATUS_ASSIGN, NodeBase.STATUS_INPROGRESS])
+                if nodes_list.count() == 0:
+                    print '----'
+                    sleep(5)
+                    nodes_list = self.get_nodes([NodeBase.STATUS_IDLE,
+                                                 NodeBase.STATUS_ASSIGN, NodeBase.STATUS_INPROGRESS])
+                    if nodes_list.count() == 0:
+                        print '\nall nodes died'
+                        break
+                        # TODO kill nodes with status 'api_limit' and run new
+            print '.',
+            sleep(0.1)
+
+        self.make_final_result()
+
+        self.mongoConn.close()
+
--- scrapers/amazon_ec2_cluster/master_run.py
+++ scrapers/amazon_ec2_cluster/master_run.py
@@ -1,45 +1,26 @@
-from time import sleep
-
 __author__ = 'Alexey'
 
 import pymongo
-from cluster_types import Task
-import cluster_types
+from cluster_types import MasterNodeBase
 
-MONGODB_IP = 'ec2-54-225-224-68.compute-1.amazonaws.com'
-DB_NAME = 'openInclude'
-NODE_SCRIPTS = ['./node_run.py', './cluster_types.py']
-SSH_INIT_COMMAND = 'sudo aptitude -y install python-setuptools; sudo easy_install pymongo stackpy'
-
-NODE_SCRIPT_PARAMS = '--dbIp=%s --dbName=%s --dbTasks=%s --dbNodes=%s' % (MONGODB_IP, DB_NAME, 'module_so_tasks', 'module_so_nodes')
-
-import sys
-sys.path.append( 'D:/Projects/ec2_tools' )
-import ec2
-
-CLUSTER_NAME = "alpha"
+# MONGODB_IP = 'ec2-54-225-224-68.compute-1.amazonaws.com'
+MONGODB_IP = 'localhost'
+DB_NAME = 'openIncludeCopy'
 CLUSTER_NODES_COUNT = 15
 
-class MasterNode():
-    def __init__(self, db_ip, db_name):
-        self.mongoConn = pymongo.MongoClient(db_ip, 27017)
-        self.db = self.mongoConn[db_name]
+class MasterNode(MasterNodeBase):
+    modules_collection_name = 'modules'
 
-    def create_task(self, tasks_collection, task_params):
-        task = {
-            'params': task_params,
-            'node_id': '',
-            'status': Task.STATUS_NEW, # assign, inprocess, completed, error
-            'result': []
-        }
-        tasks_collection.insert(task)
+    def init(self):
+        self.SSH_INIT_COMMAND += ' stackpy'
+        self.db_tasks_collection = 'module_so_tasks'
+        self.db_nodes_collection = 'module_so_nodes'
 
-    def create_tasks(self, tasks_collection, modules_collection):
-        # nodes_count = 2
-        modules_count = 20 # TODO get_all_modules_count
-
+    def add_tasks(self, tasks_collection):
+        modules_collection = self.db[self.modules_collection_name]
+        modules_count = 20
         i = 0
-        for module in modules_collection.find().sort('watchers', pymongo.DESCENDING):#.limit(modules_count):
+        for module in modules_collection.find().sort('watchers', pymongo.DESCENDING).limit(modules_count):
             name = str(module['module_name']).lower()
             print name,
             search_url_query = '*github.com/*/%s*' % name
@@ -55,113 +36,12 @@ class MasterNode():
             i+=1
         print '\nCreated %d tasks' % i
 
-    def run_cluster(self, create=True):
-        if create:
-            ec2.create(CLUSTER_NAME, CLUSTER_NODES_COUNT, 't1.micro')
-
-            cluster = None
-            while cluster == None:
-                cluster = ec2.get_cluster(CLUSTER_NAME)
-                if cluster == None:
-                    print 'cluster is creating...'
-                    sleep(5)
-
-            while 1 == ec2.ssh(CLUSTER_NAME, CLUSTER_NODES_COUNT-1, 'uname'):
-                print 'last node is starting...'
-                sleep(5)
-
-            ec2.ssh_all(CLUSTER_NAME, SSH_INIT_COMMAND, background=False)
-
-        for f in NODE_SCRIPTS:
-            ec2.scp_all(CLUSTER_NAME, f)
-
-        ec2.ssh_all(CLUSTER_NAME, 'python ' + NODE_SCRIPTS[0] + ' ' + NODE_SCRIPT_PARAMS, background=True)
-        print 'Ran cluster %s from %d nodes' % (CLUSTER_NAME, CLUSTER_NODES_COUNT)
-
-    def stop_cluster(self):
-        ec2.shutdown(CLUSTER_NAME)
-
-    def make_final_result(self):
-        pass
-
-    def get_nodes(self, statuses):
-        return self.nodes.find({
-            'status': {
-                '$in': statuses
-            }
-        })
-
-    def get_tasks(self, statuses, limit = None):
-        if limit != None:
-            return self.tasks.find({
-                'status': {
-                    '$in': statuses
-                }
-            }).limit(limit)
-        else:
-            return self.tasks.find({
-                'status': {
-                    '$in': statuses
-                }
-            })
-
-    def assign_task(self, node, task):
-        node['task_id'] = task['_id']
-        task['node_id']  = node['_id']
-        node['status'] = cluster_types.Node.STATUS_ASSIGN
-        task['status'] = cluster_types.Task.STATUS_ASSIGN
-        self.tasks.save(task)
-        self.nodes.save(node)
-        print 'Assign task %s -> node %s' % (task['_id'], node['_id'])
-
-    def run(self):
-        # self.db.drop_collection('module_so_tasks')
-        self.tasks = self.db['module_so_tasks']
-        # modules = self.db['modules']
-
-        # self.create_tasks(self.tasks, modules)
-
-        # self.db.drop_collection('module_so_nodes')
-        self.nodes = self.db['module_so_nodes']
-
-        self.run_cluster()
-
-        qus_count = self.get_tasks([Task.STATUS_COMPLETED]).count()
-        while True:
-            nodes_list = self.get_nodes([cluster_types.Node.STATUS_INIT, cluster_types.Node.STATUS_IDLE])
-            count = nodes_list.count()
-            if count > 0:
-                tasks_list = self.get_tasks([Task.STATUS_NEW], count)
-                if tasks_list.count() > 0:
-                    i=0
-                    for node in nodes_list:
-                        self.assign_task(node, tasks_list[i])
-                        qus_count+=1
-                        print qus_count,
-                        i += 1
-                        if i >= tasks_list.count():
-                            break
-                else:
-                    tasks_list = self.get_tasks([Task.STATUS_NEW, Task.STATUS_ASSIGN, Task.STATUS_INPROGRESS], 1)
-                    nodes_list = self.get_nodes([cluster_types.Node.STATUS_ASSIGN, cluster_types.Node.STATUS_INPROGRESS])
-                    if tasks_list.count() == 0 and nodes_list.count() == 0:
-                        print 'all tasks completed'
-                        break
-                    else:
-                        sleep(5)
-
-                        # TODO kill nodes with status 'api_limit' and run new
-            else:
-                sleep(1)
-        # self.stop_cluster()
-
-        self.make_final_result()
-
-        self.mongoConn.close()
-
 def main():
-    master = MasterNode(MONGODB_IP, DB_NAME)
+    master = MasterNode(MONGODB_IP, DB_NAME, CLUSTER_NODES_COUNT)
+    master.create_tasks()
+    # master.run_cluster()
     master.run()
+    # master.stop_cluster()
 
 if __name__ == "__main__":
     main()
